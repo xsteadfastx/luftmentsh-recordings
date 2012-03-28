be2f14f12feda62d29bf1c253be0c79d19a45c29
Marvin Steadfast <xsteadfastx@gmail.com>
2012-03-28 14:19:29 +0200

    splitting geojson data and map.html

---
+++ geojson.js
@@ -0,0 +1,255 @@
+var CafeRimon = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"geometry": {
+    				"type": "Point",
+        			"coordinates": [35.225, 31.777]
+			},
+			"type": "Feature",
+			"properties": {
+				"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875090&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875090&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed></object>"
+			},
+			"id": 51
+		}
+	]
+};
+
+var AsianChoir = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"geometry": {
+				"type": "Point",
+				"coordinates": [35.2176736, 31.7814996]
+			},
+			"type": "Feature",
+			"properties": {
+				"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875092&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875092&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed> </object>"
+			},
+			"id": 52
+		}
+	]
+};
+
+var BarMitzvah = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"geometry": {
+				"type": "Point",
+				"coordinates": [35.23279, 31.77568]
+			},
+			"type": "Feature",
+			"properties": {
+				"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875094%3Fsecret_token%3Ds-oBdvw&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875094%3Fsecret_token%3Ds-oBdvw&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed> </object>"
+			},
+			"id": 53
+		}
+	]
+};
+
+var GardenTomb = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"geometry": {
+				"type": "Point",
+				"coordinates": [35.23036, 31.78368]
+			},
+			"type": "Feature",
+			"properties": {
+				"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875093&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875093&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed> </object>"
+			},
+			"id": 54
+		}
+	]
+};
+
+var HolySepulchre = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"geometry": {
+				"type": "Point",
+				"coordinates": [35.2297016, 31.7785195]
+			},
+			"type": "Feature",
+			"properties": {
+				"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875103&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875103&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed> </object>"
+			},
+			"id": 55
+		}
+	]
+};
+
+var MehaneYehuda = {
+	"type": "Feature",
+   	"geometry": {
+		"type": "LineString",
+        	"coordinates": [
+			[
+				35.21272921551004,
+				31.78578303319436
+			],
+			[
+				35.21274616676217,
+				31.785621996299074
+			],
+			[
+				35.21276311801431,
+				31.785388916582217
+			],
+			[
+				35.212780069266444,
+				31.78514312342626
+			],
+			[
+				35.21279702051858,
+				31.785028702474346
+			],
+			[
+				35.21282244739678,
+				31.784880379018166
+			],
+			[
+				35.21284363646195,
+				31.784757482440185
+			],
+			[
+				35.21283939864892,
+				31.784587969918835
+			],
+			[
+				35.21287753896622,
+				31.78442693302355
+			],
+			[
+				35.21290720365745,
+				31.78429979863254
+			],
+			[
+				35.212962295226895,
+				31.78415571298939
+			],
+			[
+				35.21311485649611,
+				31.783837877011855
+			]
+		]	
+
+	},
+	"properties": {
+       		"style": {
+			color: "#3366CC",
+			weight: 6,
+       			opacity: 0.5
+       		},
+       		"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875098&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875098&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed> </object>"
+    	},
+   	"id": 1
+};
+
+var JaffaToJewish = {
+	"type": "Feature",
+	"geometry": {
+		"type": "LineString",
+    		"coordinates": [
+      			[
+				35.227318, 
+				31.776689
+			],
+			[
+				35.227650, 
+				31.776551
+			],
+			[
+				35.227996, 
+				31.776684
+			],
+			[
+				35.228577, 
+				31.776684
+			],
+			[
+				35.231166, 
+				31.776767
+			],
+			[
+				35.231179, 
+				31.776490
+			],
+			[
+				35.231103, 
+				31.776435
+			],
+			[
+				35.231051, 
+				31.776234
+			],
+			[
+				35.231010, 
+				31.775963
+			],
+			[
+				35.230953, 
+				31.775493
+			],
+			[
+				35.230907, 
+				31.775207
+			],
+			[
+				35.230890, 
+				31.774833
+			]
+     		]
+   	},
+	"properties": {
+	"style": {
+		color: "#3366CC",
+		weight: 6,
+		opacity: 0.5
+	},
+	"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875099&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875099&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed> </object>"
+	},
+	"id": 2
+};
+
+var BenYehudaStreet = {
+	"type": "Feature",
+	"geometry": {
+		"type": "LineString",
+		"coordinates": [
+			[
+				35.22014302716046,
+				31.781710949991986
+			],
+			[
+				35.21970070292838,
+				31.781999069445913
+			],
+			[
+				35.219445047638274,
+				31.781877328831577
+			],
+			[
+				35.218734894054656,
+				31.781710949991986
+			],
+			[
+				35.217687924771376,
+				31.78149993292714
+			]
+		]
+		},
+		"properties": {
+			"style": {
+				color: "#3366CC",
+				weight: 6,
+            			opacity: 0.5
+        		},
+        		"popupContent": "<object height='81' width='100%'> <param name='movie' value='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875095&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC'></param> <param name='allowscriptaccess' value='always'></param> <embed allowscriptaccess='always' height='81' src='https://player.soundcloud.com/player.swf?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F33875095&amp;show_comments=false&amp;auto_play=false&amp;color=3366CC' type='application/x-shockwave-flash' width='100%'></embed> </object>"
+		},
+    		"id": 3
+};
